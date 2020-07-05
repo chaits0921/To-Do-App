6bcef6fd149a0325db723c4f01481057d427c376
chaits0921 <cvarma@umass.edu>
2020-07-05 19:37:11 -0400

    Added interface for the todo and complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
+
+
+type todo = {
+  text: string,
+  complete: boolean,
+}
+const todos = [
+  {text: "Walk the Dog", complete: true},
+  {text: "Do Homework", complete: false}
+];
 
 function App() {
   
   return(
-    <div>Hello World!</div>
+    <TodoListItem/>
   )
 }
 
