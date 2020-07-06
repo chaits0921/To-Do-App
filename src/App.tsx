8d0cc494dc936da5bd6c76874849cde651a618a6
chaits0921 <cvarma@umass.edu>
2020-07-06 00:55:23 -0400

    Added the Input Field and Submit Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import {TodoList} from "./TodoList"
+import { AddTodoForm } from './AddTodoForm';
 type Todo = {
   text: string;
   complete: boolean;
@@ -27,10 +28,16 @@ function App() {
     });
     setTodos(newTodos);
   }
+  const addTodo:AddTodo = newTodo => {
+
+    newTodo.trim() !== "" && setTodos([...todos, {text:newTodo, complete:false}]);
+
+  };
   
   return(
     <React.Fragment>
       <TodoList todos={todos} toggleTodo={toggleTodo}/>
+      <AddTodoForm addTodo={addTodo}/>
     </React.Fragment>
       
   )
